b6fa58599aa91de89e8ab99f69e487cd10ccf0c1
Luis Falero Otiniano <falero.otiniano.luis@gmail.com>
2022-02-21 17:16:00 -0500

    2DArray, Subarray, Arraylist

---
+++ JAVA/26 - Java 2D Array.java
@@ -0,0 +1,102 @@
+/*
+You are given a  2D array. An hourglass in an array is a portion shaped like this:
+
+a b c
+  d
+e f g
+For example, if we create an hourglass using the number 1 within an array full of zeros, it may look like this:
+
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+Actually, there are many hourglasses in the array above. The three leftmost hourglasses are the following:
+
+1 1 1     1 1 0     1 0 0
+  1         0         0
+1 1 1     1 1 0     1 0 0
+The sum of an hourglass is the sum of all the numbers within it. The sum for the hourglasses above are 7, 4, and 2, respectively.
+
+In this problem you have to print the largest sum among all the hourglasses in the array.
+
+Input Format
+
+There will be exactly  lines, each containing  integers seperated by spaces. Each integer will be between  and  inclusive.
+
+Output Format
+
+Print the answer to this problem on a single line.
+
+Sample Input
+
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 2 4 4 0
+0 0 0 2 0 0
+0 0 1 2 4 0
+Sample Output
+
+19
+Explanation
+
+The hourglass which has the largest sum is:
+
+2 4 4
+  2
+1 2 4
+*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<List<Integer>> arr = new ArrayList<>();
+        List<Integer> sum = new ArrayList<>();
+        int COUNT = 2;
+
+        IntStream.range(0, 6).forEach(i -> {
+            try {
+                arr.add(
+                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        for(int i=0; i<(arr.size()-COUNT); i++) {
+            for(int j=0; j<(arr.size()-COUNT); j++) {
+                int var1 = arr.get(j).get(i);
+                int var2 = arr.get(j).get(i+1);
+                int var3 = arr.get(j).get(i+2);
+                int var4 = arr.get(j+1).get(i+1);
+                int var5 = arr.get(j+2).get(i);
+                int var6 = arr.get(j+2).get(i+1);
+                int var7 = arr.get(j+2).get(i+2);
+                sum.add(var1+var2+var3+var4+var5+var6+var7);
+            }
+        }
+        System.out.println(Collections.max(sum).toString());
+
+        bufferedReader.close();
+    }
+}
